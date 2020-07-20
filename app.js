9e0fee31cdab87a276975008dc715cb879aef3a8
Matt Spurlin <mattsp1290@gmail.com>
2020-07-19 21:35:54 -0400

    Correct objects router

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var sassMiddleware = require('node-sass-middleware');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var keysRouter = require('./routes/users');
+var objectsRouter = require('./routes/objects');
 
 var app = express();
 
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/keys', keysRouter);
+app.use('/objects', objectsRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
