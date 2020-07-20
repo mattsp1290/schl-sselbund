ae7208cd4d8bf42d3a40125dc6903addea5d28d4
Matt Spurlin <mattsp1290@gmail.com>
2020-07-19 21:47:47 -0400

    Add a python tester

---
+++ test.py
@@ -0,0 +1,8 @@
+import jwt
+import requests
+encoded_jwt = jwt.encode({'some': 'payload'}, 'secret', algorithm='HS256')
+url = 'http://localhost:3000/objects'
+headers = {'Authorization': "Bearer-{}".format(encoded_jwt)}
+
+r = requests.get(url, headers=headers)
+print(r)
